232f96e2b551e7b8f53dd90a2dba3b2327963ceb
forrestlmj <18810276930@163.com>
2019-12-15 14:28:42 +0800

    leetcode字符串题目专题 机器人

---
+++ src/main/java/StringTopic/RobotReturnToOrigin.java
@@ -0,0 +1,24 @@
+package StringTopic;
+
+public class RobotReturnToOrigin {
+    public boolean judgeCircle(String moves) {
+        int[] l = {0,0};
+        for (char c:moves.toCharArray()){
+            switch (c){
+                case 'U':
+                    l[0] += 1;
+                    break;
+                case 'D':
+                    l[0] -= 1;
+                    break;
+                case 'L':
+                    l[1] += 1;
+                    break;
+                case 'R':
+                    l[1] -= 1;
+                    break;
+            }
+        }
+        return l[0] == 0 && l[1] == 0;
+    }
+}

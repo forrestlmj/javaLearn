563a070e17d74cbac7738136ccdbc439e7d05d48
forrestlmj <18810276930@163.com>
2020-03-17 22:49:26 +0800

    链表类型题目

---
+++ src/main/java/TopInterviewQuestionsEasy/LinkedList41/LinkedList41.java
@@ -0,0 +1,28 @@
+package TopInterviewQuestionsEasy.LinkedList41;
+public class LinkedList41{
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        /**
+         * https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/41/
+         */
+    }
+}
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+}
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;        
+    }
+}

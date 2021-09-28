da9a813cdfec09e60f7be1b2da5f76b1c25e4391
forrestlmj <18810276930@163.com>
2021-09-28 21:22:28 +0800

    ip

---
+++ javaSenior/day10/src/main/java1/TcpTest1.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * 实现TCP的网络编程
+ * 例子1：客户端发送信息给服务端，服务端将数据显示在控制台上
+ *
+ * @author shkstart
+ * @create 2019 下午 3:30
+ */
+public class TcpTest1 {
+    @Test
+    public void test1(){
+        
+    }
+}

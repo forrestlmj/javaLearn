78ad722fd2e2c19fb35c86fbe7eaafd2099324d1
forrestlmj <18810276930@163.com>
2021-10-21 23:00:14 +0800

    线程不安全

--- javaSenior/practice/src/main/java/mythreadtest/ThreadUnsafeTest.java
+++ javaSenior/practice/src/main/java/mythreadtest/ThreadUnsafeTest.java
@@ -1,5 +1,7 @@
 package mythreadtest;
-class ThreadSafe implements Runnable{
+
+import org.junit.Test;
+class ThreadUnSafe implements Runnable{
     private static Integer count = 30000;
     private static Object o = new Object();
     @Override
@@ -8,7 +10,6 @@ class ThreadSafe implements Runnable{
             //https://www.cnblogs.com/williamjie/p/9466941.html
             //现象是连续一个线程在十几毫秒内都是连续能抢到的,可能和时间片有关,由于sleep不释放锁,所以,
             //即使是sleep,在之前时间片中抢到了就是抢到了,也会体现一个线程连续拿到一段时间的监视器
-            synchronized (o){
                 if(count>0){
 //                    try {
 //                        Thread.sleep(300);
@@ -20,21 +21,19 @@ class ThreadSafe implements Runnable{
                 }else break;
             }
 
-        }
     }
 }
-public class ThreadSafeTest {
-    public static void main(String[] args) {
-        ThreadSafe threadSafe = new ThreadSafe();
-        ThreadSafe threadSafe2 = new ThreadSafe();
-        Thread thread = new Thread(threadSafe);
-        thread.start();
-        thread.setName("Thread1");
-        thread.setPriority(10);
-        Thread thread2 = new Thread(threadSafe2);
-        thread2.start();
-        thread2.setName("Thread2");
-        thread2.setPriority(9);
 
+public class ThreadUnsafeTest {
+
+    public static void main(String[] args){
+        ThreadUnSafe t = new ThreadUnSafe();
+        Thread t1 = new Thread(t);
+        t1.setName("线程1");
+        t1.start();
+
+        Thread t2 = new Thread(t);
+        t2.setName("线程2");
+        t2.start();
     }
 }

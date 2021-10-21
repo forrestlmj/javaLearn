7c80d2134716d18284fb375a437cde24bcfc72e6
forrestlmj <18810276930@163.com>
2021-10-21 21:18:41 +0800

    线程通信 通过notify 与wait

--- javaSenior/practice/src/main/java/mythreadtest/ThreadCommunicationTest.java
+++ javaSenior/practice/src/main/java/mythreadtest/ThreadCommunicationTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.locks.ReentrantLock;
  * 2、调用者使用syn
  */
 class ThreadCommunication implements Runnable{
-    private static int count = 150;
+    private static int count = 100;
     private static Object o = new Object();
     @Override
     public void run() {
@@ -17,14 +17,16 @@ class ThreadCommunication implements Runnable{
             synchronized (o){
                 o.notify();
                 if(count > 0){
+                    System.out.println(Thread.currentThread().getName()
+                    +":"+count);
+                    count --;
                     if(count % 10 == 0){
                         try {
-                            o.wait(;
+                            o.wait(100);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
-                    count --;
                 }else break;
             }
 
@@ -34,6 +36,16 @@ class ThreadCommunication implements Runnable{
 }
 public class ThreadCommunicationTest {
     public static void main(String[] args) {
+        ThreadCommunication tc = new ThreadCommunication();
+        Thread thread = new Thread(tc);
+        thread.setName("Thread1");
+        thread.start();
 
+        Thread thread2 = new Thread(tc);
+        thread2.setName("Thread2");
+        thread2.start();
+        Thread thread3 = new Thread(tc);
+        thread3.setName("thread3");
+        thread3.start();
     }
 }

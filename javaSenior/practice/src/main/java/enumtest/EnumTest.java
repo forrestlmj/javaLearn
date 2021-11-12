99ce1a0b2b67405b007ae070fd00b44f4469411d
forrestlmj <18810276930@163.com>
2021-11-12 10:58:35 +0800

    enum.

--- javaSenior/practice/src/main/java/enumtest/EnumTest.java
+++ javaSenior/practice/src/main/java/enumtest/EnumTest.java
@@ -8,4 +8,11 @@ public class EnumTest {
         System.out.println(Weather.AUTUMN);
     }
 
+    @Test
+    public void testThread(){
+        System.out.println(Status.READY.getDesc());
+        for (Status value : Status.values()) {
+            System.out.println("状态可以有："+value.getName()+","+value.getDesc());
+        }
+    }
 }

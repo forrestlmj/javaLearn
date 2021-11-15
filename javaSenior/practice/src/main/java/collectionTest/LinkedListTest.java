87cc32e88a1068ca083f955cf842f72971edbe6f
forrestlmj <18810276930@163.com>
2021-11-15 22:46:58 +0800

    链表列表的jdk8源码解析

---
+++ javaSenior/practice/src/main/java/collectionTest/LinkedListTest.java
@@ -0,0 +1,15 @@
+package collectionTest;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListTest {
+    private List<String> list;
+    @Test
+    public void test(){
+        list = new LinkedList<>();
+
+    }
+}

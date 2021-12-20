f8625a37dc5484d2ea3e6725f1a19a2317eacb67
forrestlmj <18810276930@163.com>
2021-12-20 20:53:48 +0800

    栈帧与-Xss256k，在栈帧部分的stackoverflow问题

--- JVMDemo/chapter04/src/main/java/PCRegisterTest.java
+++ JVMDemo/chapter04/src/main/java/PCRegisterTest.java
@@ -7,11 +7,6 @@ public class PCRegisterTest {
     public static void main(String[] args) {
         int i = 10;
         int j = 20;
-        int k = i + j;
-
-        String s = "abc";
-        System.out.println(i);
-        System.out.println(k);
 
     }
 }

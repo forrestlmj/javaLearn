8db92da2d0d550f807888e70790cb215624fc3d2
forrestlmj <18810276930@163.com>
2021-12-20 22:28:36 +0800

    栈帧测试

---
+++ JVMDemo/chapter05/src/main/java/com/yck2/StackFrameTest.java
@@ -0,0 +1,26 @@
+package com.yck2;
+
+public class StackFrameTest {
+    public static void main(String[] args) {
+        StackFrameTest sft = new StackFrameTest();
+        sft.methodA();
+    }
+    public void methodA(){
+        System.out.println("开始methodA");
+        Long aLong = methodB();
+        System.out.println(aLong);
+        System.out.println("结束 methodA");
+    }
+    public Long methodB(){
+        Long a = 1L;
+        System.out.println("开始 methodB");
+        methodC();
+        System.out.println("结束 methodB");
+        return a;
+    }
+    public void methodC(){
+        System.out.println("开始 methodC");
+
+        System.out.println("结束 methodC");
+    }
+}

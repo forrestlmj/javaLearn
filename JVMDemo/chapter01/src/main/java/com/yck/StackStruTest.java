b1acecbecc26231baab700a5db281b5e5eee7d1b
forrestlmj <18810276930@163.com>
2021-12-21 10:52:25 +0800

    新增常量

--- JVMDemo/chapter01/src/main/java/com/yck/StackStruTest.java
+++ JVMDemo/chapter01/src/main/java/com/yck/StackStruTest.java
@@ -7,11 +7,13 @@ package com.yck;
 public class StackStruTest {
     public static void main(String[] args) throws InterruptedException {
         int i = 2;
+        char s = 's';
+        String a = "abcsss";
         int j = 3;
         int k = i + j;
 
         Thread.sleep(1000);
 
-        System.out.println(k);
+        System.out.println(a);
     }
 }

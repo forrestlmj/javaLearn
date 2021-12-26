049470ab530f37b49c51eba7006c572ef69f8c5d
forrestlmj <18810276930@163.com>
2021-12-26 09:41:45 +0800

    通过jvm参数-Xms20m,-Xmx20m来设置进程最大的堆大小

---
+++ JVMDemo/chapter08/src/main/java/com/yck/HeapDemo.java
@@ -0,0 +1,9 @@
+package com.yck;
+
+public class HeapDemo {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("start....");
+        Thread.sleep(1000*60*60);
+        System.out.println("end....");
+    }
+}

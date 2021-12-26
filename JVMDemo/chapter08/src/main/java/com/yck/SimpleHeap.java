c75fc1944222413ffc57e932b2c3e21d08d78d89
forrestlmj <18810276930@163.com>
2021-12-26 09:48:57 +0800

    堆

---
+++ JVMDemo/chapter08/src/main/java/com/yck/SimpleHeap.java
@@ -0,0 +1,20 @@
+package com.yck;
+
+public class SimpleHeap {
+    public SimpleHeap(Integer id) {
+        this.id = id;
+    }
+
+    private Integer id;
+    public void show(){
+        System.out.println("My id is " + id);
+    }
+
+    public static void main(String[] args) {
+        SimpleHeap s1 = new SimpleHeap(1);
+        SimpleHeap s2 = new SimpleHeap(2);
+
+        int[] arr =new int[10];
+        Object[] o1 = new Object[10];
+    }
+}

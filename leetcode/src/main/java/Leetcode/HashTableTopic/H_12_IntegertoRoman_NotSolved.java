2c64f083f8b976ae8147c663970458f561f97441
Chengkai Yang <yangchengkai2022@163.com>
2022-11-11 23:07:36 +0800

    int

--- leetcode/src/main/java/Leetcode/HashTableTopic/H_12_IntegertoRoman_NotSolved.java
+++ leetcode/src/main/java/Leetcode/HashTableTopic/H_12_IntegertoRoman_NotSolved.java
@@ -1,7 +1,5 @@
 package Leetcode.HashTableTopic;
 
-import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +7,7 @@ import java.util.List;
  * https://leetcode.com/problems/integer-to-roman/
  */
 class Solution_H_12{
+
     public List<Character> tmp(int n, Character c){
         List<Character> re = new ArrayList<>();
         for (int i = 0; i < n; i++) {
@@ -16,31 +15,36 @@ class Solution_H_12{
         }
         return re;
     }
-    public String intToRoman(int num) {
-        List<Character> re = new ArrayList<>();
-        int mod = 1000;
+    public Integer generate(List<Character> re, int num, int mod, char upper, char middle, char flour){
+//        if)
         int m = num / mod;
         for (int i = 0; i < m; i++) {
-            re.add('M');
+            re.add(upper);
         }
 
 
         int h_rep = (num % mod) % (mod/2);
         num = num % mod;
 
-
         if(10*num/mod < 4) {
-            tmp(h_rep,'C');
+            tmp(h_rep,flour);
         }else if(10*num/mod == 4) {
-            re.add('C');
-            re.add('D');
+            re.add(flour);
+            re.add(middle);
         } else if(10*num/mod > 4 && 10*num/mod < 9){
-            re.add('D');
-            tmp(h_rep,'C');
+            re.add(middle);
+            tmp(h_rep,flour);
         } else if(10*num/mod == 9){
-            re.add('C');
-            re.add('M');
+            re.add(flour);
+            re.add(upper);
         }
+        return num;
+    }
+    public String intToRoman(int num) {
+        List<Character> re = new ArrayList<>();
+        num = generate(re,num,1000,'M','D','C');
+        num = generate(re,num,100,'C','L','X');
+        num = generate(re,num,10,'X','V','I');
 
         StringBuilder sb = new StringBuilder();
         for (Character character : re) {
@@ -49,7 +53,7 @@ class Solution_H_12{
         return sb.toString();
     }
 }
-public class H_12_IntegertoRoman {
+public class H_12_IntegertoRoman_NotSolved {
     public static void main(String[] args) {
         System.out.println(300%1000);
     }

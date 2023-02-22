a4f811d856a56119ce5f4277a2c5a8f4f9baeed4
Chengkai Yang <yangchengkai2022@163.com>
2023-02-22 20:59:43 +0800

    UML: association

---
+++ JavaDesignPattern/src/main/java/com/github/yck/uml/association/Association.java
@@ -0,0 +1,16 @@
+package com.github.yck.uml.association;
+
+import java.util.List;
+
+public class Association {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+class Card{}
+
+/**
+ * 这里就是练习，一对多
+ */
+class Person{ private List<Card> cards;}
+

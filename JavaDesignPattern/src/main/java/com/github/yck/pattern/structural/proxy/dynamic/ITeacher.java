94622fa32f7374feddbf7a250fec8cf239e44f77
Chengkai Yang <yangchengkai2022@163.com>
2023-03-01 22:44:31 +0800

    [代理模式] 动态代理

---
+++ JavaDesignPattern/src/main/java/com/github/yck/pattern/structural/proxy/dynamic/ITeacher.java
@@ -0,0 +1,6 @@
+package com.github.yck.pattern.structural.proxy.dynamic;
+
+public interface ITeacher {
+    void teach();
+    void sayHello();
+}
---
+++ JavaDesignPattern/src/main/java/com/github/yck/pattern/structural/proxy/dynamic/Teacher.java
@@ -0,0 +1,13 @@
+package com.github.yck.pattern.structural.proxy.dynamic;
+
+public class Teacher implements ITeacher {
+    @Override
+    public void teach() {
+        System.out.println("Teacher is teaching.");
+    }
+
+    @Override
+    public void sayHello() {
+        System.out.println("Teacher say hello.");
+    }
+}

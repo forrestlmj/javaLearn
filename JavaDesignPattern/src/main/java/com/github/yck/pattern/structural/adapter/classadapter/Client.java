dae7cddeee099ea33a2028d1fc2006f5fa8c2ba7
Chengkai Yang <yangchengkai2022@163.com>
2023-03-01 15:05:07 +0800

    [适配器模式]类适配器模式，比较直观

---
+++ JavaDesignPattern/src/main/java/com/github/yck/pattern/structural/adapter/classadapter/Client.java
@@ -0,0 +1,8 @@
+package com.github.yck.pattern.structural.adapter.classadapter;
+
+public class Client {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        phone.charging(new VoltageAdapter220V5V());
+    }
+}
---
+++ JavaDesignPattern/src/main/java/com/github/yck/pattern/structural/adapter/classadapter/Voltage5V.java
@@ -0,0 +1,5 @@
+package com.github.yck.pattern.structural.adapter.classadapter;
+
+public interface Voltage5V {
+    public int output5v();
+}

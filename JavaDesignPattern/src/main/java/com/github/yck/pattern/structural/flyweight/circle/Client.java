cb0811e9db96797cd1b28b1eeee94c8c33596465
Chengkai Yang <yangchengkai2022@163.com>
2023-03-03 17:54:45 +0800

    [Flyweight 模式] 完成

---
+++ JavaDesignPattern/src/main/java/com/github/yck/pattern/structural/flyweight/circle/Client.java
@@ -0,0 +1,21 @@
+package com.github.yck.pattern.structural.flyweight.circle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Client {
+    public static void main(String[] args) {
+        Random r = new Random();
+        List<String> colors = new ArrayList<String >(5){{
+            add("Red");add("Black");add("Blue");add("Yellow");add("Brown");
+        }};
+
+        for (int i = 0; i < 20; i++) {
+            Circle c = CircleFactory.getCircle(colors.get(r.nextInt(colors.size()-1)));
+            c.setX(r.nextInt(100));
+            c.setY(r.nextInt(100));
+            c.draw();
+        }
+    }
+}
---
+++ JavaDesignPattern/src/main/java/com/github/yck/pattern/structural/flyweight/circle/Shape.java
@@ -0,0 +1,5 @@
+package com.github.yck.pattern.structural.flyweight.circle;
+
+public interface Shape {
+    void draw();
+}

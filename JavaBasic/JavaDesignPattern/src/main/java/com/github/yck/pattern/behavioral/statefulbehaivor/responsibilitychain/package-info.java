0296cba81d95da96727bfdb08db0f3516171487b
Chengkai Yang <yangchengkai2022@163.com>
2023-03-14 22:32:48 +0800

    [责任链模式] UML

--- JavaBasic/JavaDesignPattern/src/main/java/com/github/yck/pattern/behavioral/statefulbehaivor/responsibilitychain/package-info.java
+++ JavaBasic/JavaDesignPattern/src/main/java/com/github/yck/pattern/behavioral/statefulbehaivor/responsibilitychain/package-info.java
@@ -1,4 +1,9 @@
 package com.github.yck.pattern.behavioral.statefulbehaivor.responsibilitychain;
 /**
  * 责任链模式
+ * 比较直观，就是针对请求，按照 链式进行处理
+ * 类似于 报销审批、流程审批、请求处理等
+ * 核心概念
+ *  1、将请求的发送端与接收端解耦，使用一个链表对请求轮番处理
+ *  2、如果链表上的节点无法处理请求，那么交给下一个节点
  */

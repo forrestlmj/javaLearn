3bb2d5053a7bce66b540b3df7fae85e05a445174
yangchengkai <yangchengkai2022@163.com>
2024-06-03 20:02:54 +0800

    [GREEDY] 没做出来，看答案写的

--- Algorithms/courses/baricourse/baricourse/src/main/java/com/github/yck/greedy/Greedy763.java
+++ Algorithms/courses/baricourse/baricourse/src/main/java/com/github/yck/greedy/Greedy763.java
@@ -11,73 +11,36 @@ import java.util.stream.Collectors;
  * @version 1.0
  * @date 2024/5/30 19:59
  * https://leetcode.com/problems/partition-labels/description/
+ * 比较好的题目
  * 贪心原则：
- * 1. 计算出每个字符的开头和结束位置后，按顺序合并，
+ * 1. 第一次遍历字符串，首先计算出每个字母出现的最后位置。
+ * 2. 第二次遍历字符串，为了决定如何分区，分区的贪心原则是，当前遍历的字母的最后位置与当前分区的最后位置做比较，取最大的。如果遍历到的索引为当前的分区结束位置，则断开。
  */
 
-class TupleYck{
-    @Override
-    public String toString() {
-        return "TupleYck{" +
-                "startIdx=" + startIdx +
-                ", endIdx=" + endIdx +
-                '}';
-    }
-    public Integer getSize(){
-        return this.endIdx - this.startIdx + 1;
-    }
-    public TupleYck(Integer startIdx, Integer endIdx) {
-        this.startIdx = startIdx;
-        this.endIdx = endIdx;
-    }
 
-    Integer startIdx;
-    Integer endIdx;
-}
 class Solution764 {
-    public Boolean canMerge(TupleYck a, TupleYck b){
-        return !(b.startIdx > a.endIdx);
-    }
-    public TupleYck merge(TupleYck a, TupleYck b){
-        return new TupleYck(Math.min(a.startIdx,b.startIdx),Math.max(a.endIdx,b.endIdx));
-    }
+
     public List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        Map<Character,Integer> m = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            m.put(c,i);
+        }
+        int end = -1;
+        int partitionLen = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            Integer lastIndexOfC = m.get(c);
+            end = Math.max(end,lastIndexOfC);
+            partitionLen ++;
+            if (i == end){
 
-        List<List<Integer>> subs = new ArrayList<>();
-        Map<Character,TupleYck> m = new LinkedHashMap<>();
-        char[] charArray = s.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
-            char c = charArray[i];
-            if(m.containsKey(c)){
-                m.get(c).endIdx = i;
-            }else {
-                TupleYck tupleYck = new TupleYck(i,i);
-
-                m.put(c,tupleYck);
+                result.add(partitionLen);
+                partitionLen = 0;
             }
         }
-        System.out.println(m);
-        Iterator<Character> iterator = m.keySet().iterator();
-        Character firstC = iterator.next();
-        List<TupleYck> result = new ArrayList<>();
-        TupleYck first = m.get(firstC);
-        while (iterator.hasNext()){
-            TupleYck second = m.get(iterator.next());
-
-            if(canMerge(first,second)){
-                first = merge(first,second);
-                if (!iterator.hasNext()){
-                    result.add(first);
-                }
-            }else {
-                result.add(first);
-                first = second;
-            }
-        }
-        List<Integer> collect = result.stream().map(
-                t -> t.getSize()
-        ).collect(Collectors.toList());
-        return  collect;
+        return result;
     }
 }
 public class Greedy763 {
@@ -90,8 +53,11 @@ public class Greedy763 {
         System.out.println(i.substring(0,9));
         System.out.println(i.substring(9,16));
         System.out.println(i.substring(16,24));
-
+//        s.partitionLabels("caedbdedda");
+//        s.partitionLabels("caedbdedda");
         Assert.equals(s.partitionLabels("ababcbacadefegdehijhklij"), Arrays.asList(9,7,8));
+        Assert.equals(s.partitionLabels("caedbdedda"), Arrays.asList(1,9));
+        Assert.equals(s.partitionLabels("eaaaabaaec"), Arrays.asList(9,1));
         Assert.equals(s.partitionLabels("eccbbbbdec"), Arrays.asList(10));
     }
 }
